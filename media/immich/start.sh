10032075a2d67d50d36e8dceaac3b52741ab0c2e
Dwi Elfianto <2359896+delfianto@users.noreply.github.com>
2026-03-27 19:54:09 +0700

    Just YOLO it to make immich runs on rootless docker with scaffold.sh

--- media/immich/start.sh
+++ media/immich/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose --env-file ~/.config/docker/compose.env --env-file .env --env-file .env.local up
+docker compose --env-file ~/.config/docker/compose.env --env-file .env --env-file .env.local up --force-recreate
